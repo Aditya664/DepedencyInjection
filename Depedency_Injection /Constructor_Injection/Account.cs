e339ae16ca2c8d621e65902d17663da971d5d4b9
aditya664 <adityadeshmukh7350@gmail.com>
2022-09-25 12:29:05 +0530

    New

--- Depedency_Injection /Constructor_Injection/Account.cs
+++ Depedency_Injection /Constructor_Injection/Account.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DepedencyInjection.Depedency_Injection.Constructor
+namespace DepedencyInjection.Depedency_Injection.Constructor_Injection
 {
     public class Account
     {
