abd8b3e46b310621a9949092d2f9c13633fe5079
aditya664 <adityadeshmukh7350@gmail.com>
2022-09-25 18:59:33 +0530

    gitignore

---
+++ Depedency_Injection /Property_Injection/CurrentAccount.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepedencyInjection.Depedency_Injection .Property_Injection
+{
+    public class CurrentAccount
+    {
+        
+    }
+}
